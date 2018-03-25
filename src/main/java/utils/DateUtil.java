71171011ebd033da825f299451fd2763ef730c3c
clhost <lchostinc@gmail.com>
2018-03-25 03:46:36 +0300

    update

--- src/main/java/utils/DateUtil.java
+++ src/main/java/utils/DateUtil.java
@@ -8,9 +8,30 @@ public class DateUtil {
     public static long fromHHMMToLong(String hhmm) {
         String[] tokens = hhmm.split(":");
 
-        int h = Integer.parseInt(tokens[0]);
-        int m = Integer.parseInt(tokens[1]);
+        long h = Integer.parseInt(tokens[0]);
+        long m = Integer.parseInt(tokens[1]);
 
         return h * 3600 + m * 60;
+    }
+
+    public static String fromLongToHHMM(long hhmm) {
+        long h = hhmm / 3600;
+        long m = (hhmm - (h * 3600)) / 60;
+
+        String th, tm;
+
+        if (h / 10 == 0) {
+            th = "0" + h;
+        } else {
+            th = String.valueOf(h);
+        }
+
+        if (m / 10 == 0) {
+            tm = "0" + m;
+        } else {
+            tm = String.valueOf(m);
+        }
+
+        return th + ":" + tm;
     }
 }
