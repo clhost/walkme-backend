229c52a6095a9d04bc4140fc70c5321aca346cab
clhost <lchostinc@gmail.com>
2018-03-26 23:35:11 +0300

    category for Place entity

---
+++ src/main/java/storage/entities/Category.java
@@ -0,0 +1,46 @@
+package storage.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wm_category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "category_name", unique = true, nullable = false)
+    private String categoryName;
+
+    public Category(int id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public Category() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+}
