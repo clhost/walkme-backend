1b8f49edcaa01159de3225802e6a5e94db41c61f
clhost <lchostinc@gmail.com>
2018-03-27 14:42:08 +0300

    own overriding dialect

---
+++ src/main/java/storage/config/UTFMySQLSupportDialect.java
@@ -0,0 +1,10 @@
+package storage.config;
+
+import org.hibernate.dialect.MySQL57Dialect;
+
+public class UTFMySQLSupportDialect extends MySQL57Dialect {
+    @Override
+    public String getTableTypeString() {
+        return "default charset=utf8 COLLATE utf8_general_ci";
+    }
+}
