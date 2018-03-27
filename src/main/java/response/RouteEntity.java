f583dafd58450dbf660102eb42f6e553e992b303
clhost <lchostinc@gmail.com>
2018-03-27 18:10:06 +0300

    route entity for client

---
+++ src/main/java/response/RouteEntity.java
@@ -0,0 +1,72 @@
+package response;
+
+import storage.entities.Day;
+import storage.entities.Location;
+import storage.entities.Place;
+import utils.DateUtil;
+
+import java.time.LocalDateTime;
+
+public class RouteEntity {
+    private Location point;
+    private String category;
+    private int categoryId;
+    private String address;
+    private String addressAdditional;
+    private String workingTime;
+
+    private RouteEntity(Location location,
+                String category,
+                int categoryId,
+                String address,
+                String addressAdditional,
+                String workingTime) {
+        this.point = location;
+        this.category = category;
+        this.categoryId = categoryId;
+        this.address = address;
+        this.addressAdditional = addressAdditional;
+        this.workingTime = workingTime;
+    }
+
+    public static RouteEntity of(Place place) {
+        LocalDateTime date = LocalDateTime.now();
+        int dayOfWeek = date.getDayOfWeek().getValue();
+
+        long start = place.getSchedule().getScheduleInfo().get(Day.getByDayOfWeek(dayOfWeek)).getStart();
+        long finish = place.getSchedule().getScheduleInfo().get(Day.getByDayOfWeek(dayOfWeek)).getFinish();
+
+        return new RouteEntity(
+                place.getLocation(),
+                place.getGisCategory(),
+                place.getCategory().getId(),
+                place.getAddressName(),
+                place.getAddressComment(),
+                "с " + DateUtil.fromLongToHHMM(start) + " по " + DateUtil.fromLongToHHMM(finish)
+                );
+    }
+
+    public Location getPoint() {
+        return point;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getAddressAdditional() {
+        return addressAdditional;
+    }
+
+    public String getWorkingTime() {
+        return workingTime;
+    }
+}
