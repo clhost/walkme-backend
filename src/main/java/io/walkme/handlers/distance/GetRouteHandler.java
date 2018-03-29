55f30d9419a8a9bc83c510ce0fa1f6c812a90411
clhost <lchostinc@gmail.com>
2018-03-29 15:13:11 +0300

    base route handler with stub response

---
+++ src/main/java/io/walkme/handlers/distance/GetRouteHandler.java
@@ -0,0 +1,92 @@
+package io.walkme.handlers.distance;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.walkme.response.RouteBuilder;
+import io.walkme.response.RouteEntity;
+import io.walkme.services.GenericEntityService;
+import io.walkme.services.PlaceService;
+import io.walkme.services.SessionService;
+import io.walkme.services.UserService;
+import io.walkme.services.fields.PlaceFields;
+import io.walkme.storage.entities.*;
+import io.walkme.utils.ResponseBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * handle: /api/getRoute
+ * params (json): token, lat, lng, categories
+ *
+ *
+ */
+public class GetRouteHandler extends ChannelInboundHandlerAdapter {
+    private static final String API_PREFIX = "api";
+    private static final String API_GET_ROUTE = "getRoute";
+
+    private static final String PARAM_TOKEN = "token";
+    private static final String PARAM_LAT = "lat";
+    private static final String PARAM_LNG = "lng";
+    private static final String PARAM_CATEGORIES = "categories";
+
+    private static final GenericEntityService<User, String  > userService = new UserService();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (!(msg instanceof FullHttpRequest)) {
+            ctx.fireChannelRead(msg);
+            return;
+        }
+
+        FullHttpRequest request = (FullHttpRequest) msg;
+
+        QueryStringDecoder decoder = new QueryStringDecoder(request.uri());
+        String[] tokens = decoder.path().substring(1).split("/");
+        Map<String, List<String>> params = decoder.parameters();
+
+
+        if (tokens.length < 2) {
+            ctx.fireChannelRead(msg);
+        } else if (params.get("token") != null && params.get("token").size() > 0 &&
+                        SessionService.getInstance().isSessionExist(params.get("token").get(0))) {
+            if (tokens[0].equals(API_PREFIX) && tokens[1].equals(API_GET_ROUTE)) {
+                handleRoute(ctx, params);
+            }
+        } else {
+            ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(HttpResponseStatus.OK, "{ \"error:\" unauthorized }"));
+        }
+
+        ctx.close();
+    }
+
+    private void handleRoute(ChannelHandlerContext ctx, Map<String, List<String>> params) throws Exception {
+        PlaceService placeService = new PlaceService();
+
+        Place place1 = placeService.get
+                ("5348552838479901_nehhg2p8p713845B56IG0GGGlszk9z26G6G433A4G5" +
+                                "BA04HArgewB4979B3IG22G0J5CI5G4k4gyuvG45516384A7991H3J2H42",
+                        PlaceFields.ID);
+        Place place2 = placeService.get
+                ("5348552838480147_ep9zwip8p7136598532GGGGcxt4pbu26G6G43399G5" +
+                                "357J51rgewB4979A8IG1I0G8G5I4GJ8yoiuvG455169396B2H1H48A6",
+                        PlaceFields.ID);
+
+        List<RouteEntity> entities = new ArrayList<>();
+        entities.add(RouteEntity.of(place1));
+        entities.add(RouteEntity.of(place2));
+
+        List<Location> locs = new ArrayList<>();
+        locs.add(new Location(30.333, 30.1232));
+        locs.add(new Location(30.333, 30.1232));
+        locs.add(new Location(30.333, 30.1232));
+
+        ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
+                HttpResponseStatus.OK, RouteBuilder.asJson(200, entities, locs)));
+    }
+}
