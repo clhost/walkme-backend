157c4c5d320b713afdb04176e09e1d8d3f27d4fd
clhost <lchostinc@gmail.com>
2018-03-29 18:51:03 +0300

    drop schema helper

---
+++ src/main/java/io/walkme/storage/Dropper.java
@@ -0,0 +1,26 @@
+package io.walkme.storage;
+
+import io.walkme.utils.HibernateUtil;
+import org.hibernate.Session;
+
+public class Dropper {
+    public static void drop() {
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+
+            session.createNativeQuery("drop table wm_session").executeUpdate();
+            session.createNativeQuery("drop table wm_place").executeUpdate();
+            session.createNativeQuery("drop table wm_user").executeUpdate();
+            session.createNativeQuery("drop table wm_category").executeUpdate();
+
+            session.getTransaction().commit();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
