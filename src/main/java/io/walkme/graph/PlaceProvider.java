2fb0efdcd5102f346f67eb8e838454dd66600a34
clhost <lchostinc@gmail.com>
2018-03-31 21:20:19 +0300

    данные для теста, сойдет и двух точек

--- src/main/java/io/walkme/graph/PlaceProvider.java
+++ src/main/java/io/walkme/graph/PlaceProvider.java
@@ -15,16 +15,10 @@ public class PlaceProvider {
 
     static {
         try {
-            places.add(service.get("5348552838479901_nehhg2p8p713845B56IG0GGGlszk" +
-                    "9z26G6G433A4G5BA04HArgewB4979B3IG22G0J5CI5G4k4gyuvG45516384A7991H3J2H42", PlaceFields.ID));
-            places.add(service.get("5348552838482231_wfsos6p8p713845B56IG0GGGm76B" +
-                    "1r26G6G433A3G54334H7rgewB4979B2IG16G0J5CI5G4mfscuvG45516384A7A88H3J2Hgb", PlaceFields.ID));
-            places.add(service.get("5348552838495522_jqjrnxp8p7136598532GGGG68cxf" +
-                    "tc26G6G4339AG5732J63rgewB4979A9IG2I0G8G5I4GJyitkuvG455169396C7H1H48Ad", PlaceFields.ID));
-            places.add(service.get("5348552838557427_mqr48xp8p713845B563GG0GGd57mD" +
-                    "826G6G433A5G5952AH2rgewB4979B4IG24G0J18BJ2JlyfAuvG45516384A87193J3HHH23", PlaceFields.ID));
-            places.add(service.get("5348552838746636_7luvwDp8p713845B563GG0GGA3yp6" +
-                    "826G6G433A5G58739H2rgewB4979B4IG26G0J18BJ2J7p2yuvG45516384AA6279J3HHH8e", PlaceFields.ID));
+            places.add(service.get("5348552838481127_Bhqy9lp8p714845B563GG0GGvndmi" +
+                    "A26G6G44549G5322AH2rgewB415158IG2AG0J196J2Jbm2duvG45528384A7A497J3HHH7i", PlaceFields.ID));
+            places.add(service.get("5348552838504355_jBrnnqp8p714845B563GG0GGu8nuk" +
+                    "526G6G44547G56458H2rgewB415156IG27G0J196J2Jfp4iuvG45528384A82329J3HHH55", PlaceFields.ID));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,7 +26,7 @@ public class PlaceProvider {
 
     public static Place randomPlace() {
         Random random = new Random();
-        int p = random.nextInt(4);
+        int p = random.nextInt(places.size());
 
         return places.get(p);
     }
