8d5e9281286fb25da1b7aa9e3f8d214dd740aac7
clhost <lchostinc@gmail.com>
2018-03-31 18:15:05 +0300

    fix

--- src/main/java/io/walkme/handlers/route/GetRouteHandler.java
+++ src/main/java/io/walkme/handlers/route/GetRouteHandler.java
@@ -8,8 +8,8 @@ import io.netty.handler.codec.http.QueryStringDecoder;
 import io.walkme.core.GlobalProps;
 import io.walkme.graph.PlaceProvider;
 import io.walkme.graph.stub.RouteFinder;
-import io.walkme.response.RouteBuilder;
-import io.walkme.response.RouteEntity;
+import io.walkme.response.route.RouteBuilder;
+import io.walkme.response.route.RouteEntity;
 import io.walkme.services.GenericEntityService;
 import io.walkme.services.PlaceService;
 import io.walkme.services.SessionService;
@@ -53,22 +53,9 @@ public class GetRouteHandler extends ChannelInboundHandlerAdapter {
 
         if (tokens.length < 2) {
             ctx.fireChannelRead(msg);
-        } else if (tokens[0].equals(API_PREFIX) && tokens[1].equals(API_GET_ROUTE)) {
-            if (params.get("token") != null && params.get("token").size() > 0 &&
-                    SessionService.getInstance().isSessionExist(params.get("token").get(0))) {
-                handleRoute(ctx, params);
-            } else {
-                ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
-                        HttpResponseStatus.FORBIDDEN,
-                        ResponseBuilder.JSON_UNAUTHORIZED_REQUEST));
-            }
-        } else {
-            ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
-                    HttpResponseStatus.BAD_REQUEST,
-                    ResponseBuilder.JSON_BAD_REQUEST));
+        } else if (tokens[0].equals(API_PREFIX) && tokens[1].equals(API_GET_ROUTE)){
+            handleRoute(ctx, params);
         }
-
-        ctx.close();
     }
 
     private void handleRoute(ChannelHandlerContext ctx, Map<String, List<String>> params) throws Exception {
@@ -84,30 +71,11 @@ public class GetRouteHandler extends ChannelInboundHandlerAdapter {
 
             ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
                     HttpResponseStatus.OK, RouteBuilder.asJson(200, entities, points)));
+            ctx.close();
         } else {
             ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
                     HttpResponseStatus.OK, ResponseBuilder.JSON_STUB_BAD_REQUEST));
+            ctx.close();
         }
-
-        /*Place place1 = placeService.get
-                ("5348552838479901_nehhg2p8p713845B56IG0GGGlszk9z26G6G433A4G5" +
-                                "BA04HArgewB4979B3IG22G0J5CI5G4k4gyuvG45516384A7991H3J2H42",
-                        PlaceFields.ID);
-        Place place2 = placeService.get
-                ("5348552838480147_ep9zwip8p7136598532GGGGcxt4pbu26G6G43399G5" +
-                                "357J51rgewB4979A8IG1I0G8G5I4GJ8yoiuvG455169396B2H1H48A6",
-                        PlaceFields.ID);
-
-        List<RouteEntity> entities = new ArrayList<>();
-        entities.add(RouteEntity.of(place1));
-        entities.add(RouteEntity.of(place2));
-
-        List<Location> locs = new ArrayList<>();
-        locs.add(new Location(30.333, 30.1232));
-        locs.add(new Location(30.333, 30.1232));
-        locs.add(new Location(30.333, 30.1232));
-
-        ctx.writeAndFlush(ResponseBuilder.buildJsonResponse(
-                HttpResponseStatus.OK, RouteBuilder.asJson(200, entities, locs)));*/
     }
 }
