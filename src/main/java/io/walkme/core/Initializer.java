e9a1f90cb8d139e4abf65467f184e3f84e8aca0e
clhost <lchostinc@gmail.com>
2018-04-02 16:02:35 +0300

    добавлен логаут, перераспределены воркер треды

--- src/main/java/io/walkme/core/Initializer.java
+++ src/main/java/io/walkme/core/Initializer.java
@@ -1,10 +1,11 @@
 package io.walkme.core;
 
 import io.walkme.handlers.auth.AuthHandler;
+import io.walkme.handlers.auth.LogoutHandler;
 import io.walkme.handlers.auth.TokenHandler;
 import io.walkme.handlers.categories.GetCategoriesHandler;
 import io.walkme.handlers.route.GetRouteHandler;
-import io.walkme.handlers.test.InfoHandler;
+import io.walkme.handlers.info.InfoHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -16,7 +17,8 @@ import io.netty.util.concurrent.EventExecutorGroup;
 
 public class Initializer extends ChannelInitializer<SocketChannel> {
     private final EventExecutorGroup route = new DefaultEventExecutorGroup(8);
-    private final EventExecutorGroup auth = new DefaultEventExecutorGroup(8);
+    private final EventExecutorGroup auth = new DefaultEventExecutorGroup(4);
+    private final EventExecutorGroup logout = new DefaultEventExecutorGroup(4);
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
@@ -30,6 +32,7 @@ public class Initializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast("info", new InfoHandler(true));
         pipeline.addLast("token", new TokenHandler());
         pipeline.addLast(auth, "auth", new AuthHandler());
+        pipeline.addLast(logout, "logout", new LogoutHandler());
         pipeline.addLast("categories", new GetCategoriesHandler());
         pipeline.addLast(route, "route", new GetRouteHandler());
     }
