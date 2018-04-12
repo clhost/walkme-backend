f3bf0de0989d17822209dd05cf01becd6d150a67
clhost <lchostinc@gmail.com>
2018-04-12 22:22:50 +0300

    временное хранилище всех нод

---
+++ src/main/java/io/walkme/core/PlaceHolder.java
@@ -0,0 +1,80 @@
+package io.walkme.core;
+
+import io.walkme.graph.prod.Node;
+import io.walkme.services.GenericEntityService;
+import io.walkme.services.PlaceService;
+import io.walkme.services.fields.PlaceFields;
+import io.walkme.storage.entities.Category;
+import io.walkme.storage.entities.Place;
+import io.walkme.storage.entities.WalkMeCategory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+// FIXME КОСТЫЛЬНЫЙ PLACE HOLDER - видит node класс - зависит от него
+public class PlaceHolder {
+    //private static final ConcurrentHashMap<Category, ArrayList<Node>> nodes = new ConcurrentHashMap<>();
+    private static final List<Node> nodeList = new ArrayList<>();
+    private static final GenericEntityService<Place, String> service = new PlaceService();
+    private static volatile boolean isLoaded = false;
+
+    private static final Logger logger = LogManager.getLogger(PlaceHolder.class);
+
+    public static void load() {
+        if (!isLoaded) {
+            Thread printer = new Thread(() -> {
+                while (!Thread.interrupted()) {
+                    try {
+                        System.out.print("\rLoading to RAM.");
+                        Thread.sleep(300);
+                        System.out.print("\rLoading to RAM..");
+                        Thread.sleep(300);
+                        System.out.print("\rLoading to RAM...");
+                        Thread.sleep(300);
+                        System.out.print("\rLoading to RAM....");
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        logger.info("LoadPrinter thread interrupted.");
+                        return;
+                    }
+                }
+            }, "LoadPrinter");
+
+            printer.start();
+
+            try {
+                List<String> criteria = new ArrayList<>();
+
+                for (WalkMeCategory e : WalkMeCategory.getAll()) {
+                    criteria.add(String.valueOf(e.id()));
+                }
+
+                List<Place> listPlaces = service.getAll(criteria, PlaceFields.CATEGORY_ID);
+
+                for (Place p : listPlaces) {
+                    /*if (nodes.get(p.getCategory()) == null) {
+                        nodes.put(p.getCategory(), new ArrayList<>(Collections.singletonList(Node.of(p))));
+                    } else {
+                        nodes.get(p.getCategory()).add(Node.of(p));
+                    }*/
+                    nodeList.add(Node.of(p));
+                }
+            } catch (Exception e) {
+                logger.error("Can't load to RAM. Message: " + e.getMessage());
+                printer.interrupt();
+            }
+
+            printer.interrupt();
+            System.out.println("OK");
+            isLoaded = true;
+        }
+    }
+
+    public static List<Node> getAll() {
+        return nodeList;
+    }
+}
