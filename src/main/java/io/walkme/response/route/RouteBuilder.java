4cce065a6c444e9aef6dd0135eed2075337c7031
clhost <lchostinc@gmail.com>
2018-04-15 18:28:31 +0300

    новая структура ответа

--- src/main/java/io/walkme/response/route/RouteBuilder.java
+++ src/main/java/io/walkme/response/route/RouteBuilder.java
@@ -8,20 +8,33 @@ import java.util.List;
 public class RouteBuilder {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();//new Gson();
 
-    public static String asJson(int status, List<RouteEntity> entities, List<Location> points) {
+    public static String asJson(int status, List<RouteEntity> entities, List<List<Location>> points) {
         JsonObject jsonObject = new JsonObject();
 
+        Way[] ways = new Way[points.size()];
+        for (int i = 0; i < ways.length; i++) {
+            ways[i] = new Way(points.get(i));
+        }
+
         String placePoints = gson.toJson(entities);
-        String allPoints = gson.toJson(points);
+        String allPoints = gson.toJson(ways);
 
         JsonObject places = new JsonObject();
         places.add("places", new JsonParser().parse(placePoints));
-        places.add("points", new JsonParser().parse(allPoints));
+        places.add("ways", new JsonParser().parse(allPoints));
 
         jsonObject.addProperty("status", status);
         jsonObject.add("result", places);
 
         return gson.toJson(jsonObject);
         //return jsonObject.toString();
+    }
+
+    static class Way {
+        private List<Location> points;
+
+        Way(List<Location> points) {
+            this.points = points;
+        }
     }
 }
