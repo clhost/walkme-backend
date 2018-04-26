eb0d53affbd495ddaa9cec5073374e11a3e378da
clhost <lchostinc@gmail.com>
2018-04-26 19:37:28 +0300

    изменилось название пропертей

--- auth_module/src/main/java/auth/helpers/ConfigHelper.java
+++ auth_module/src/main/java/auth/helpers/ConfigHelper.java
@@ -1,6 +1,6 @@
 package auth.helpers;
 
 public class ConfigHelper {
-    public static final String LOCAL_PROPERTIES = "local.properties";
-    public static final String HIBERNATE_PROPERTIES = "hibernate.properties";
+    public static final String LOCAL_PROPERTIES = "auth_local.properties";
+    public static final String HIBERNATE_PROPERTIES = "auth_hibernate.properties";
 }
